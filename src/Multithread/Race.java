05b613c993685654b3cd9a308d34b580acad94a9
91797 <mansikumari7970@gmail.com>
2020-11-13 18:57:44 +0530

    tortoise and rabbit code

--- src/Multithread/Race.java
+++ src/Multithread/Race.java
@@ -4,10 +4,9 @@ package Multithread;
 
 public class Race {
     public static void main(String[] args) {
-            Rabbit rabbit = new Rabbit();
-            rabbit.getRabbit().start();
-            Tortoise tortoise=new Tortoise();
-            tortoise.getTortoise().start();
-        }
+        Rabbit rabbit = new Rabbit();
+        rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.start();
     }
-
+}
--- src/Multithread/Tortoise.java
+++ src/Multithread/Tortoise.java
@@ -1,42 +1,60 @@
 package Multithread;
 
-public class Tortoise extends Rabbit {
-    private static final int MILESTONE = 3;
-    private final Thread tortoise;
+import javax.swing.*;
 
-    public Tortoise() {
-        this.tortoise = new Thread(this, "Tortoise");
+public class Tortoise extends Thread {
 
-    }
+        public static final int MILESTONES = 5;
 
-    public Thread getTortoise() {
-        return tortoise;
-    }
+        private final Thread tortoise;
 
-    public void run() {
-        for (int index = 0; index < MILESTONES; index++) {
-            switch (index) {
-                case 0:
-                    System.out.println("The tortoise has started from the start line!");
-                    break;
-                case 1:
-                    System.out.println("The tortoise has reached the Cool River!");
-                    break;
-                case 2:
-                    System.out.println("The tortoise has reached the Mountain Hill!");
-                    break;
-                case 3:
-                    System.out.println("The tortoise has reached the Big Oak Tree!");
-                    System.out.println("The tortoise is leading and hence is going to sleep!");
+        public Tortoise() {
 
-                case 4:
-                    System.out.println("The tortoise has reached the finish line!");
-                    break;
+            this.tortoise= new Thread(this, "tortoise");
+        }
+
+        public Thread getThread() {
+            return tortoise;
+        }
+
+        @Override
+        public void run() {
+            for (int index = 0; index < MILESTONES; index++) {
+                switch (index) {
+                    case 0:
+                        System.out.println("The tortoise has started from the start line!");
+                        break;
+                    case 1:
+                        System.out.println("The tortoise has reached the Cool River!");
+                        break;
+                    case 2:
+                        System.out.println("The tortoise has reached the Mountain Hill!");
+                        break;
+                    case 3:
+                        System.out.println("The tortoise has reached the Big Oak Tree!");
+                        break;
+                    case 4:
+                        System.out.println("the tortoise has reached the finished line!");
+                        break;
+                }
+
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException e) {
+                    System.err.println("The tortoise has lost the path!");
+                }
             }
 
 
+            JOptionPane.showConfirmDialog(
+                    null,
+                    "The tortoise has completed the race!",
+                    "tortoise",
+                    JOptionPane.OK_CANCEL_OPTION
+            );
         }
     }
-}
+
+
 
 
